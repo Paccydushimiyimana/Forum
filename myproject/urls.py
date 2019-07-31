7738e07a56f282e5b2a17787de0bda03a64580f3
Paccymuhoza <dushimepaccy@gmail.com>
2019-07-31 13:18:48 +0200

    dealing with topics template/urls(regex) and views(function)

--- myproject/urls.py
+++ myproject/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 from boards import views
@@ -21,4 +22,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home/', views.home, name='home'),
     path('admin/', admin.site.urls),
+    path(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
 ]
